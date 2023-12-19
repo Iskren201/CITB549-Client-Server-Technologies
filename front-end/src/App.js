a51c857fd94072a0ab61d09a4cf4ebdf030bb5d9
Iskren201 <iskren201@gmail.com>
2023-12-19 15:10:34 +0200

    commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 
 import { BrowserRouter, Routes, Route, Link, Router } from "react-router-dom";
@@ -14,25 +14,57 @@ import { Login } from "./Components/Login/Login";
 import { Register } from "./Components/Register/Register";
 // h-screen
 
-
 function App() {
-    return (
-        <BrowserRouter>
-            <NavBar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/Login" element={<Login />} />
-                <Route path="/Register" element={<Register />} />
-                <Route path="/About" element={<About />} />
-                <Route path="/Comments" element={<Comments />} />
-                <Route path="/Contact" element={<Contact />} />
-                <Route path="/Gallery" element={<Gallery />} />
-                <Route path="*" element={<Error />} />
-            </Routes>
-            <Footer />
-        </BrowserRouter >
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [userEmail, setUserEmail] = useState("");
 
-    );
-};
+  const handleLogin = (email) => {
+    // Вашата логика за успешен вход
+    setLoggedIn(true);
+    setUserEmail(email);
+  };
+
+  const handleLogout = () => {
+    // Вашата логика за изход
+    setLoggedIn(false);
+    setUserEmail("");
+  };
+
+  //   return (
+  //     <BrowserRouter>
+  //       <NavBar />
+  //       <Routes>
+  //         <Route path="/" element={<Home />} />
+  //         <Route path="/Login" element={<Login />} />
+  //         <Route path="/Register" element={<Register />} />
+  //         <Route path="/About" element={<About />} />
+  //         <Route path="/Comments" element={<Comments />} />
+  //         <Route path="/Contact" element={<Contact />} />
+  //         <Route path="/Gallery" element={<Gallery />} />
+  //         <Route path="*" element={<Error />} />
+  //       </Routes>
+  //       <Footer />
+  //     </BrowserRouter>
+  //   );
+
+  return (
+    <BrowserRouter>
+      {isLoggedIn && <NavBar />}{" "}
+      <Routes>
+        <Route
+          path="/"
+          element={isLoggedIn ? <Home /> : <Login setLoggedIn={setLoggedIn} />}
+        />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Comments" element={<Comments />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Gallery" element={<Gallery />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      {isLoggedIn && <Footer />}{" "}
+    </BrowserRouter>
+  );
+}
 
 export default App;
--- front-end/src/Components/Login/Login.js
+++ front-end/src/Components/Login/Login.js
@@ -1,64 +1,103 @@
 import React, { useState } from "react";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-export const Login = () => {
+export const Login = ({ setLoggedIn }) => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
+  const handleLogin = (e) => {
+    e.preventDefault();
 
+    // Добавете логика за валидация на входните данни
+    const isValid = validateFormData(formData);
 
-    return (
-        <>
-            <div
-                className="flex h-screen w-full items-center justify-center bg-gray-900 bg-cover bg-no-repeat"
-                style={{
-                    backgroundImage:
-                        'url("https://images.unsplash.com/photo-1499123785106-343e69e68db1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1748&q=80")',
-                }}
-            >
-                <div className="rounded-xl bg-gray-800 bg-opacity-50 px-16 py-10 shadow-lg backdrop-blur-md max-sm:px-8">
-                    <div className="text-white">
-                        <div className="mb-8 flex flex-col items-center">
-                            <img
-                                // src="https://www.logo.wine/a/logo/Instagram/Instagram-Glyph-Color-Logo.wine.svg"
-                                width={150}
-                                alt=""
-                                srcSet=""
-                            />
-                            <h1 className="mb-2 text-2xl">PetCommunity Login</h1>
-                            <span className="text-gray-300">Enter Login Details</span>
-                        </div>
-                        <form action="#" >
-                            <div className="mb-4 text-lg">
-                                <input
-                                    className="rounded-3xl border-none bg-yellow-400 bg-opacity-50 px-6 py-2 text-center text-inherit placeholder-slate-200 shadow-lg outline-none backdrop-blur-md"
-                                    type="text"
-                                    name="name"
+    if (isValid) {
+      // Тук може да добавите код за изпращане на заявка към сървъра за вход
+      // Ако входът е успешен, извикайте setLoggedIn(true);
+      setLoggedIn(true);
 
-                                    placeholder="id@email.com"
-                                />
-                            </div>
-                            <div className="mb-4 text-lg">
-                                <input
-                                    className="rounded-3xl border-none bg-yellow-400 bg-opacity-50 px-6 py-2 text-center text-inherit placeholder-slate-200 shadow-lg outline-none backdrop-blur-md"
-                                    type="Password"
-                                    name="name"
+      // Използвайте toast за показване на съобщение за успешен вход
+      toast.success("Login successful!");
+    } else {
+      // Ако входът не е успешен, използвайте toast за показване на съобщение за грешка
+      toast.error("Invalid email or password. Please try again.");
+    }
+  };
 
-                                    placeholder="*********"
-                                />
-                            </div>
-                            <div className="mt-8 flex justify-center text-lg text-black">
-                                <button
-                                    type="submit"
-                                    className="rounded-3xl bg-yellow-400 bg-opacity-50 px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-yellow-600"
-                                >
-                                    Login
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
+  const validateFormData = ({ email, password }) => {
+    // Тук можете да добавите валидация на входните данни, например:
+    // Проверка за валиден email, дължина на паролата и други
+
+    // Пример:
+    if (!email || !password) {
+      return false;
+    }
+
+    return true;
+  };
+
+  return (
+    <>
+      <div
+        className="flex h-screen w-full items-center justify-center bg-gray-900 bg-cover bg-no-repeat"
+        style={{
+          backgroundImage:
+            'url("https://images.unsplash.com/photo-1499123785106-343e69e68db1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1748&q=80")',
+        }}
+      >
+        <div className="rounded-xl bg-gray-800 bg-opacity-50 px-16 py-10 shadow-lg backdrop-blur-md max-sm:px-8">
+          <div className="text-white">
+            <div className="mb-8 flex flex-col items-center">
+              <img width={150} alt="" srcSet="" />
+              <h1 className="mb-2 text-2xl">PetCommunity Login</h1>
+              <span className="text-gray-300">Enter Login Details</span>
             </div>
-        </>
-    );
+            <form action="#">
+              <div className="mb-4 text-lg">
+                <input
+                  className="rounded-3xl border-none bg-yellow-400 bg-opacity-50 px-6 py-2 text-center text-inherit placeholder-slate-200 shadow-lg outline-none backdrop-blur-md"
+                  type="text"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  placeholder="id@email.com"
+                />
+              </div>
+              <div className="mb-4 text-lg">
+                <input
+                  className="rounded-3xl border-none bg-yellow-400 bg-opacity-50 px-6 py-2 text-center text-inherit placeholder-slate-200 shadow-lg outline-none backdrop-blur-md"
+                  type="Password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                  placeholder="*********"
+                />
+              </div>
+              <div className="mt-8 flex justify-center text-lg text-black">
+                <button
+                  type="submit"
+                  className="rounded-3xl bg-yellow-400 bg-opacity-50 px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-yellow-600"
+                  onClick={handleLogin}
+                >
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <ToastContainer />
+    </>
+  );
 };
