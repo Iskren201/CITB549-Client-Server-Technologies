dc077d57e499208312e14dd1bca412d4b52d72e0
Iskren201 <iskren201@gmail.com>
2024-02-01 20:29:33 +0200

    Back-end-with-CS

---
+++ back-endWithCs/WebApplication3/WebApplication3/JsonDataAccess.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
+using WebApplication3.Models;
+
+namespace WebApplication3.Data
+{
+    public class JsonDataAccess
+    {
+        private readonly string _filePath;
+
+        public JsonDataAccess(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public List<CommentModel> GetComments()
+        {
+            if (File.Exists(_filePath))
+            {
+                string json = File.ReadAllText(_filePath);
+                return JsonConvert.DeserializeObject<List<CommentModel>>(json);
+            }
+
+            return new List<CommentModel>();
+        }
+
+        public void SaveComments(List<CommentModel> comments)
+        {
+            string json = JsonConvert.SerializeObject(comments);
+            File.WriteAllText(_filePath, json);
+        }
+    }
+}
---
+++ back-endWithCs/WebApplication3/WebApplication3/Models/CommentModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication3.Models
+{
+    public class CommentModel
+    {
+        public string Name { get; set; }
+        public string Text { get; set; }
+    }
+}
